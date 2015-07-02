6bbb38962fc90528ef912fb06fa4cb6fd76b2397
Damian Wynne <damian.wynne2@mail.dcu.ie>
2015-07-02 01:04:30 +0100

    tidied up code/added some comments

--- dst_processor.rb
+++ dst_processor.rb
@@ -8,65 +8,45 @@ def _usage
 	puts "  html_out_dir:       Directory to store generated HTML files"
 end
 
+#
+# Open specified file and attempt to parse as XML and generate new REXML Document
+#
 def _loadXML(file)
 	begin
-		return Document.new(File.open(file))
+		xmlFile = File.open(file)
+		xmlObj = Document.new(xmlFile)
+		xmlFile.close
 	rescue => e
 		puts "ERROR: Unable to create XML object from file: #{file}"
 		puts e.message
 		puts e.backtrace
 		File.close(file)
-		exit!
+		exit 1
+	end
+
+	return xmlObj
+end
+
+#
+# Create the HTML file for the specified location in the specified directory
+#
+def _createHTMLFile(locName, content, outDir)
+    File.open("#{outDir}/#{locName}.html", 'w') do |f|
+        f.write($headerText)
+		f.write("<h1>Lonely Planet: #{locName}</h1>")
+	    f.write($navTitle)
+		f.write($navigationHTML)
+        f.write($blockTitle)
+		f.write("<h1><li class='first'><a href='#'>#{locName}</a></li></h1>")
+		f.write($mainBlock)
+	    f.write(content)
+        f.write($closeHTML)
 	end
 end
 
-def _createHTMLFile(id, locName, content, outDir)
-	outFile = File.new("#{outDir}/#{locName}.html", 'w')
-	outFile.write('<!DOCTYPE html>')
-	outFile.write('<html>')
-	outFile.write('<head>')
-	outFile.write('<meta http-equiv="content-type" content="text/html; charset=UTF-8">')
-	outFile.write('<title>Lonely Planet</title>')
-	outFile.write('<link href="static/all.css" media="screen" rel="stylesheet" type="text/css">')
-	outFile.write('</head>')
-	outFile.write('<body>')
-	outFile.write('<div id="container">')
-	outFile.write('<div id="header">')
-	outFile.write('<div id="logo">')
-        outFile.write("<h1>Lonely Planet: #{locName}</h1>")
-      	outFile.write('</div>')
-      	outFile.write('<div id="wrapper">')
-        outFile.write('<div id="sidebar">')
-        outFile.write('<div class="block">')
-        outFile.write('<h3>Navigation</h3>')
-        outFile.write('<div class="content">')
-        outFile.write('<div class="inner">')
-        outFile.write(@navigationHTML)
-        outFile.write('</div>')
-        outFile.write('</div>')
-        outFile.write('</div>')
-        outFile.write('</div>')
-        outFile.write('<div id="main">')
-	outFile.write('<div class="block">')
-        outFile.write('<div class="secondary-navigation">')
-        outFile.write('<ul>')
-        outFile.write("<h1><li class='first'><a href='#'>#{locName}</a></li></h1>")
-        outFile.write('</ul>')
-        outFile.write('<div class="clear"></div>')
-        outFile.write('</div>')
-        outFile.write('<div class="content">')
-        outFile.write('<div class="inner">')
-       	outFile.write(content)
-        outFile.write('</div>')
-        outFile.write('</div>')
-        outFile.write('</div>')
-	outFile.write('</div>')
-	outFile.write('</div>')
-	outFile.write('</body>')
-	outFile.write('</html>')
-	outFile.close
-end
-
+#
+# Iterate through the hierarchy and generate the HTML files for each location
+#
 def _generateHTML(locHier, outDir, parent=nil)
 	locHier.each do |loc|
 		id = loc[:id]
@@ -76,7 +56,7 @@ def _generateHTML(locHier, outDir, parent=nil)
 			_generateHTML(loc[:sub_loc], outDir, locName)
 		end
 		
-		loc_content = @destinations.elements["destination[@atlas_id='#{id}']"]
+		loc_content = $destinations.elements["destination[@atlas_id='#{id}']"]
 		
 		content = ""
 		loc_content.elements.each do |el|	
@@ -84,40 +64,46 @@ def _generateHTML(locHier, outDir, parent=nil)
 			section_title = el.elements[1].name.split('_').each { |word| word.capitalize! }.join(" ")	
 			content += "<h2>#{section_title}</h2>"
 			content += "<p>"
-			content += el.elements[1].elements[1].cdatas()[0].to_s
+            content_string = el.elements[1].elements[1].cdatas()[0].to_s
+			content += content_string.gsub("\n\n\n", "<br><p>").gsub("\n\n", "<p>").gsub("\n", "<br>")
 			content += "<p>"
 		end
 		
-		_createHTMLFile(id, locName, content, outDir)
+		_createHTMLFile(locName, content, outDir)
 	end
 end
 
+#
+# Generate the location hierarchy from the taxonomy XML
+#
 def _generateHierarchy(node, id=nil)
 	loc = Hash.new
-	@navigationHTML += "<ul><li>"
+	$navigationHTML += "<ul><li>"
 	node.each_element do |element|
 		
 		if element.name == "node_name" and !id.nil?
 			loc[:id] = id
 			loc[:location] = element.text
 			loc[:sub_loc] = Array.new
-			@navigationHTML += "<a href='#{loc[:location]}.html'>#{loc[:location]}</a>"
+			$navigationHTML += "<a href='#{loc[:location]}.html'>#{loc[:location]}</a>"
 			
 		else
 			loc[:sub_loc] << _generateHierarchy(element, element.attributes["atlas_node_id"])
 		end
 		
 	end
-@navigationHTML += "</li></ul>"
+	$navigationHTML += "</li></ul>"
 	
 	return loc
 end
 
-
+#
+# Main program begins here
+#
 if ARGV.length != 3
 	puts "Not enough args"
 	_usage
-	exit!	
+	exit 1
 end
 
 destinationFile = ARGV[0]
@@ -127,13 +113,13 @@ outputDir = ARGV[2]
 if !File.exist?(destinationFile)
 	puts "Destination File: #{destinationFile} does not exist"
 	_usage
-	exit!
+	exit 1
 end
 
 if !File.exist?(taxonomyFile)
 	puts "Taxonomy File: #{taxonomyFile} does not exist"
 	_usage
-	exit!
+	exit 1
 end
 
 if !Dir.exist?(outputDir)
@@ -144,18 +130,87 @@ end
 destinationXML = _loadXML(destinationFile)
 taxonomyXML = _loadXML(taxonomyFile)
 
+#
+# Get the root node of both XML objects
+# destinations is made global as we want to access it when generating the content for each page
+#
 taxonomies = taxonomyXML.root
-@destinations = destinationXML.root
+$destinations = destinationXML.root
 
 locationHierarchy = Array.new
-@navigationHTML = "<nav>"
+
+#
+# navigationHTML contains the navigation code- made global as it is the same for each page 
+# (can access any location from any page)
+#
+$navigationHTML = "<nav>"
+
+#
+# Create the html content which is the same for each page as globals
+#
+$headerText = "<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv='content-type' content='text/html; charset=UTF-8'>
+        <title>Lonely Planet</title>
+        <link href='static/all.css' media='screen' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <div id='container'>
+            <div id='header'>
+                <div id='logo'>"
+
+$navTitle = "
+                </div>
+                <div id='wrapper'>
+                    <div id='sidebar'>
+                        <div class='block'>
+                            <h3>Navigation</h3>
+                            <div class='content'>
+                                <div class='inner'>"
+
+$blockTitle = "
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id='main'>
+                        <div class='block'>
+                            <div class='secondary-navigation'>
+                                <ul>"
+
+$mainBlock = "
+                                </ul>
+                                <div class='clear'></div>
+                            </div>
+                            <div class='content'>
+                                <div class='inner'>"
+
+$closeHTML = "
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>"
+
+#
+# Generation of the location hierarchy makes some assumptions on the xml format -
+#   If the node has the attribute atlas_node_id, then it is a location, and will have at least one child node.
+#   That child node will be node_name, and contain the name of the location.
+#   Any other child nodes will be locations within the node location region 
+#   e.g. the node for South Africa contains the child node -> <node_name>South Africa</node_name>, as well as a child node for Cape Town.
+#
 taxonomies.elements[1].each_element do |el|
 	if el.attributes.include?("atlas_node_id")
 		locationHierarchy << _generateHierarchy(el, el.attributes["atlas_node_id"])
 	end
 end
-@navigationHTML += "</nav>"
+$navigationHTML += "</nav>"
 
 _generateHTML(locationHierarchy, outputDir)
 
-
+exit
